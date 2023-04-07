ee40254f140a2649bdd93c4c8840f988babc1eb4
daphnehe <114850865+daphnehe@users.noreply.github.com>
2023-04-07 13:19:06 -0700

    Add files via upload

---
+++ pieces.java
@@ -0,0 +1,59 @@
+public class pieces {
+    private int xCor;
+    private int yCor;
+    private boolean king;
+    private String color;
+
+    public pieces()
+    {
+        xCor = 0;
+        yCor = 0;
+        king = false;
+        color = " _ ";
+    }
+
+    public pieces( int x, int y, boolean b, String c)
+    {
+        xCor = x;
+        yCor = y;
+        king = b;
+        color = c;
+    }
+
+    public int getX()
+    {
+        return xCor;
+    }
+
+    public int getY()
+    {
+        return yCor;
+    }
+
+    public boolean kingStatus()
+    {
+        return king;
+    }
+
+    public String getColor()
+    {
+        return color;
+    }
+
+    public void setX(int x)
+    {
+        xCor = x;
+    }
+
+    public void setY(int y)
+    {
+        yCor = y;
+    }
+
+    public void makeKing(boolean k)
+    {
+        king = k;
+    }
+
+    public void setColor(String c) { color = c; }
+}
